1beffb58a916c4adb8d705fd6b705c35a9577282
Ian <Ian2020@users.noreply.github.com>
2019-06-13 13:26:47 +0100

    Look for a settings file locally first, to facilitate testing

--- src/postings.sh
+++ src/postings.sh
@@ -3,7 +3,12 @@ BEGIN {
   # TODO: Can we use a better separator? Just picked something unlikely to appear in regex
   SAVED_FS=FS
   FS="¬"
-  SETTINGS_FILE=ENVIRON["HOME"] "/.ledgerimport"
+  # Look for a local settings file otherwise try home
+  if(system("test -f .ledgerimport" ) == 0) {
+    SETTINGS_FILE=".ledgerimport"
+  } else {
+    SETTINGS_FILE=ENVIRON["HOME"] "/.ledgerimport"
+}
   while(getline < SETTINGS_FILE) {
     if(!match($0,"^#")) {
       substitutions[a]=$1
